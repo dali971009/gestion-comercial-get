1b862c2cffacefc3a45b6b2e392c4134aff53551
dali <dali971009@gmail.com>
2023-11-25 16:49:44 -0500

    Improve print contract

--- server/src/controllers/contract-controller.ts
+++ server/src/controllers/contract-controller.ts
@@ -96,9 +96,8 @@ export const useContractController = () => {
     }
   }
 
-  async function getLogoImage() {
-    const logoFilePath: string = '../assets/logo-get.jpg';
-    const fileHandle: number = fs.openSync(__dirname + '/' + logoFilePath, 'r');
+  async function getLogoImage(path: string) {
+    const fileHandle: number = fs.openSync(__dirname + '/' + path, 'r');
 
     const stats: fs.Stats = fs.fstatSync(fileHandle);
     const fileSize: number = stats.size;
@@ -119,32 +118,126 @@ export const useContractController = () => {
     // Convert HTML to PDF using PDF-LIB
     const pdfDoc = await PDFDocument.create();
     const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-    const page = pdfDoc.addPage();
+    const frontPage = pdfDoc.addPage();
     const page2 = pdfDoc.addPage();
 
-    page.drawText('Example', {
-      font,
-    });
+    const { width, height } = frontPage.getSize();
 
-    const { width, height } = page.getSize();
+    const logoImage = await getLogoImage('../assets/logo-get.png');
+    const qrImage = await getLogoImage('../assets/qr.png');
+    const logoEmbedPng = await pdfDoc.embedPng(logoImage);
+    const qrEmbedPng = await pdfDoc.embedPng(qrImage);
 
-    const logoImage = await getLogoImage();
-    const embedJpg = await pdfDoc.embedJpg(logoImage);
-
-    for (const page of pdfDoc.getPages()) {
+    for (let i = 1; i < pdfDoc.getPages().length; i++) {
+      const page = pdfDoc.getPage(i);
+      page.drawImage(logoEmbedPng, {
+        x: 100,
+        y: height - 80,
+        height: 40,
+        width: 100,
+      });
       page.drawText(`UEB GET: DATOS`, {
         x: width - 215,
         y: height - 65,
         size: 15,
         color: rgb(0.2, 0.2, 0.2),
       });
-      page.drawImage(embedJpg, {
-        x: 100,
-        y: height - 80,
-        height: 40,
-        width: 100,
-      });
     }
+
+    const title = 'Contrato de comodato';
+    const number = 'No. AA-BBB-CC-DDD';
+    const clientName = 'Cliente: _______________________________';
+    const clientNumber = 'No. del Cliente: ___________';
+    const ueb = 'UEB GET: _______________';
+    const address = 'Calle O No. 157 e/ 23 y Humboldt, Vedado, Plaza de la Revolución, La Habana. Cuba.';
+    const phone = 'Teléf.: +537 3677577, +537 8378423.   Callcenter: 18800';
+    const email = 'E-mail: cac@get.tur.cu    Sitio Web: http://get.tur.cu';
+
+    const titleWidth = font.widthOfTextAtSize(title.toUpperCase(), 28);
+    const numberWidth = font.widthOfTextAtSize(number.toUpperCase(), 20);
+    const clientNameWidth = font.widthOfTextAtSize(clientName, 16);
+    const clientNumberWidth = font.widthOfTextAtSize(clientNumber, 13);
+    const uebWidth = font.widthOfTextAtSize(ueb, 16);
+    const addressWidth = font.widthOfTextAtSize(address, 9);
+    const phoneWidth = font.widthOfTextAtSize(phone, 9);
+    const emailWidth = font.widthOfTextAtSize(email, 9);
+
+    // print logo
+    frontPage.drawImage(logoEmbedPng, {
+      x: (width - 160) / 2,
+      y: height - 120,
+      height: 60,
+      width: 160,
+    });
+    // print title
+    frontPage.drawText(title.toUpperCase(), {
+      x: (width - titleWidth) / 2,
+      y: height - 220,
+      font,
+      size: 28,
+    });
+    // print number
+    frontPage.drawText(number.toUpperCase(), {
+      x: (width - numberWidth) / 2,
+      y: height - 300,
+      font,
+      size: 20,
+    });
+    // print client name
+    frontPage.drawText(clientName, {
+      x: (width - clientNameWidth) / 2,
+      y: height - 380,
+      font,
+      size: 16,
+    });
+    // print client number
+    frontPage.drawText(clientNumber, {
+      x: (width - clientNameWidth) / 2 + clientNameWidth - clientNumberWidth,
+      y: height - 440,
+      font,
+      size: 13,
+    });
+    // print ueb
+    frontPage.drawText(ueb, {
+      x: (width - uebWidth) / 2,
+      y: height - 540,
+      font,
+      size: 16,
+    });
+
+    const spaceBetweenQrAndText = 15;
+    const qrWidth = 40;
+    const qrX = (width - (qrWidth + spaceBetweenQrAndText + addressWidth)) / 2;
+    const footerTextX = qrX + qrWidth + spaceBetweenQrAndText;
+
+    // print qr
+    frontPage.drawImage(qrEmbedPng, {
+      x: qrX,
+      y: 50,
+      height: qrWidth,
+      width: qrWidth,
+    });
+    // print address
+    frontPage.drawText(address, {
+      x: footerTextX,
+      y: 80,
+      font,
+      size: 9,
+    });
+    // print address
+    frontPage.drawText(phone, {
+      x: footerTextX,
+      y: 65,
+      font,
+      size: 9,
+    });
+    // print address
+    frontPage.drawText(email, {
+      x: footerTextX,
+      y: 50,
+      font,
+      size: 9,
+    });
 
     // Send the generated PDF as a response
     const pdfBytes = await pdfDoc.saveAsBase64({ dataUri: true });
